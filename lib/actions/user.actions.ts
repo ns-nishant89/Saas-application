f2ff6c19fbf2c7727ff82c6bb4684cb6fd79bcb4
NISHANT SUMAN <132561602+ns-nishant89@users.noreply.github.com>
2025-01-06 21:38:04 +0530

    Update user.actions.ts

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,7 +2,7 @@
 
 import { revalidatePath } from "next/cache";
 
-import User from "../database/models/user.model.ts";
+import User from "../databases/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 
